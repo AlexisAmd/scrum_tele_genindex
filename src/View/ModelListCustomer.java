ef925139dead4a794eb2c91e5fb949d991c32402
Cephou <laurent.noyon@gmail.com>
2017-05-17 16:43:01 +0200

    ohlala

--- src/View/ModelListCustomer.java
+++ src/View/ModelListCustomer.java
@@ -1,11 +1,14 @@
 package View;
 
 import javax.swing.ComboBoxModel;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.event.ListDataListener;
 
 import Main.Main;
+import Model.Customer;
+import Model.SpecieCategory;
 
-public class ModelListCustomer implements ComboBoxModel {
+public class ModelListCustomer extends DefaultComboBoxModel {
 
 	private Main main;
 	
@@ -14,46 +17,6 @@ public class ModelListCustomer implements ComboBoxModel {
 		for(Customer c : main.getTheCustomers()) {
 			this.addElement(c);
 		}
-	}
-
-	public ModelListCustomer(Main main) {
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public void addListDataListener(ListDataListener l) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public Object getElementAt(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getSize() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void removeListDataListener(ListDataListener l) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public Object getSelectedItem() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setSelectedItem(Object anItem) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
