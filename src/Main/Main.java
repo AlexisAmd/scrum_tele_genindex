68c0f042a6173fe434078f3d85a0fc9acba9f3ca
Cephou <laurent.noyon@gmail.com>
2017-05-17 18:45:25 +0200

    oula.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -211,6 +211,8 @@ public class Main {
 		
 		// Ajoute la commande à la liste des commandes du client
 		selectedCustomer.addOrder(nOrder);
+		
+		JOptionPane.showMessageDialog(new JFrame(), "The order has been created.");
 
 	}
 		
