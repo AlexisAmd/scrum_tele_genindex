27d2f31b1fa4fadd98aa26b5b69b15d56d337713
Alexis A <alexis.amiand@gmail.com>
2017-05-17 16:13:16 +0200

    modellistcustomer

---
+++ src/View/ModelListCustomer.java
@@ -0,0 +1,48 @@
+package View;
+
+import javax.swing.ComboBoxModel;
+import javax.swing.event.ListDataListener;
+
+public class ModelListCustomer implements ComboBoxModel {
+
+	public ModelListCustomer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void addListDataListener(ListDataListener l) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object getElementAt(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void removeListDataListener(ListDataListener l) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object getSelectedItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setSelectedItem(Object anItem) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
